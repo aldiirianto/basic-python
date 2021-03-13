29354f6a45097dea8c4ed91004d7b9e0cf0eb670
aldiirianto <aldiirianto999@gmail.com>
2021-03-13 14:56:42 +0700

    Pertemuan 1

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#This is a comment :p
+print("Hello World!")
+print("My name is Aldi.")
+a = 5
+b = 6
+c = a + b
+print(c)
+c = c + 4
+print(c)
+d = "Life is like a boat"
+print(d)
+e = "Yes or Not?"
+print(d+e)
